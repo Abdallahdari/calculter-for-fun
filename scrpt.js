32925e174511ad1dae4c8237b1a461ed2d7e1bc1
abdallah <abdallahabdirisaaq@gmail.com>
2024-04-09 00:47:04 +0300

    first calculter

--- scrpt.js
+++ scrpt.js
@@ -4,6 +4,7 @@
     const equal = document.querySelector('.equal');
     const numbers = document.querySelectorAll('.number');
     const operators = document.querySelectorAll('.operator');
+   const delet1=document.querySelector('.delete1')
 
     DeleteAll.addEventListener('click', function() {
         display.value = "";
@@ -12,7 +13,6 @@
     equal.addEventListener('click', function() {
         display.value = eval(display.value);
     });
-
     numbers.forEach(function(number) {
         number.addEventListener('click', function() {
             display.value += this.value;
@@ -25,3 +25,29 @@
         });
     });
 
+    delet1.addEventListener('click', function() {
+
+    // we are using only this part for accesisng the current value before modifying 
+        let currentValue = display.value;
+        
+        if (currentValue.length > 0) {
+            // this is for deleteting the last index and restoring other element to new array 
+            display.value = currentValue.slice(0, -1)
+        }})
+
+        document.addEventListener('keydown', function(v) {
+            if (v.key === 'Escape') {
+                let currentValue = display.value;
+                if (currentValue.length > 0) {
+                    display.value = currentValue.slice(0, -1);
+                }
+            }
+        });
+
+
+        numbers.forEach(function(input){
+            input.addEventListener('click',function(){
+                display.value+=this.value
+            })
+        })
+        
